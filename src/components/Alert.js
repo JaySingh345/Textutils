6d11a7d43954e85757127bf9558b9de81ca8292d
jay singh <jaysingh94127@gmail.com>
2025-07-05 19:00:00 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,7 +6,7 @@ function Alert(props) {
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
   return (
-    props.alert && <div className={`alert alert-{props.alert.type} alert-dismissible fade show` }color = 'blue' role="alert">
+    props.alert && <div className={`alert alert-{props.alert.type} alert-dismissible fade show` } role="alert">
         <strong>{capitalize(props.alert.type)}</strong>{props.alert.msg}
     </div>
   )
